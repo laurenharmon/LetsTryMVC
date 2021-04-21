1eaf7cff05058ddfdb6c60d88c2de0f3f2e4a2dc
Lauren Harmon <harmon.lauren@hotmail.com>
2021-04-21 16:15:18 -0500

    already friends working

--- Controllers/FriendsListController.cs
+++ Controllers/FriendsListController.cs
@@ -23,11 +23,25 @@ namespace LetsTryMVC.Controllers
         }
         public IActionResult Index()
         {
-            var friends = _context.Users
+            var people = _context.Users.ToList();
+            var friends = _context.Friends
+                .Include(x => x.User)
+                .Include(x => x.Friend)
                 .ToList();
-            var self = friends.Find(x => x.UserName == User.Identity.Name);
-            friends.Remove(self);
-            return View(friends);
+            //Remove friends I already have from list
+            ViewBag.friendNames = new List<string>();
+            foreach(var relationship in friends)
+            {
+                if(User.Identity.Name == relationship.User.UserName)
+                {
+                    ViewBag.friendNames.Add(relationship.Friend.UserName);
+                }
+            }
+
+            //Remove myself from friends options
+            var self = people.Find(x => x.UserName == User.Identity.Name);
+            people.Remove(self);
+            return View(people);
         }
 
         public UserProfile GetUser()
